6d7340c098427cd1edee9f4de299917869000c83
Restly <liknlie@gmail.com>
2020-11-27 11:15:03 +0900

    CreateCommonScript,FixSomething

---
+++ Assets/Scripts/Scene/StageSelectController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Common;
+
+public class StageSelectController : MonoBehaviour
+{
+    // フェード用キャンバス
+    private GameObject fadeCanvas;
+
+    // フェード用スクリプト
+    private Fadecontroller fadeScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeCanvas = GameObject.Find("FadeCanvas");
+        fadeScript = fadeCanvas.GetComponent<Fadecontroller>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ←キーが押されたら
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+
+        }
+        // →キーが押されたら
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+        }
+        // スペースキーが押されたら
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+        }
+        // Escキーが押されたら
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            fadeScript.fadeOutStart(Common.Scene.TITLE_SCENE);
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/Common.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Common
+{
+    public static class Scene
+    {
+        public const int TITLE_SCENE        = 0;
+        public const int STAGE_SELECT_SCENE = 1;
+        public const int PLAY_SCENE         = 2;
+        public const int RESULT_SCENE       = 3;
+    }
+}
