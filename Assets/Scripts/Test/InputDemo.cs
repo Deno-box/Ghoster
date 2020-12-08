4366cbde0e8d6fe45e3f932c63578955c3492d2d
liknlie7 <liknlie@gmail.com>
2020-12-08 12:29:40 +0900

    TitleSceneComplete

---
+++ Assets/Scripts/Test/InputDemo.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var a = Input.GetAxis("ThirdAxis");
+        //if (a!=0)
+        //{
+        //    Debug.Log("3rdAxis" + a.ToString());
+        //}
+        var h = Input.GetAxis("CrossHorizontal");
+        var v = Input.GetAxis("CrossVertical");
+        if (h!=0)
+        {
+            Debug.Log("CrossHorizontal" + h.ToString());
+        }
+        if (v != 0)
+        {
+            Debug.Log("CrossVertical" + v.ToString());
+        }
+        if (Input.GetKeyDown("joystick button 0"))
+        {
+            Debug.Log("button0");
+        }
+        if (Input.GetKeyDown("joystick button 1"))
+        {
+            Debug.Log("button1");
+        }
+        if (Input.GetKeyDown("joystick button 2"))
+        {
+            Debug.Log("button2");
+        }
+        if (Input.GetKeyDown("joystick button 3"))
+        {
+            Debug.Log("button3");
+        }
+        if (Input.GetKeyDown("joystick button 4"))
+        {
+            Debug.Log("button4");
+        }
+        if (Input.GetKeyDown("joystick button 5"))
+        {
+            Debug.Log("button5");
+        }
+        if (Input.GetKeyDown("joystick button 6"))
+        {
+            Debug.Log("button6");
+        }
+        if (Input.GetKeyDown("joystick button 7"))
+        {
+            Debug.Log("button7");
+        }
+        if (Input.GetKeyDown("joystick button 8"))
+        {
+            Debug.Log("button8");
+        }
+        if (Input.GetKeyDown("joystick button 9"))
+        {
+            Debug.Log("button9");
+        }
+
+        float hori = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        if ((hori != 0) || (vert != 0))
+        {
+            Debug.Log("stick:" + hori + "," + vert);
+        }
+    }
+}
