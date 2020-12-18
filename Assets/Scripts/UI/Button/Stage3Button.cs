96664db3083358593cad9e2dcf1548712612682a
kkm-sgr <kakami.suguru@gmail.com>
2020-12-18 12:23:56 +0900

    AddButtonFocus

--- Assets/Scripts/UI/Button/Stage3Button.cs
+++ Assets/Scripts/UI/Button/Stage3Button.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class Stage3Button : MonoBehaviour
 {
     // ボタンをクリックした時の処理
     public void OnClick()
     {
-
         // シーンを呼び出す
         Debug.Log("Button3 click!");
         SceneManager.LoadScene("DemoPlayScene");
-
     }
 }
