12d17d5f0038063b846d7d8494dfa518eadd7c6b
yuki <N.Yuuki2021>
2021-01-05 16:45:22 +0900

    tmp

---
+++ Assets/Scripts/Editor/PlayerMoveDataEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerMovePath))]
+public class PlayerMoveDataEditor : Editor
+{
+    public void OnEnable()
+    {
+        PlayerMovePath data = (PlayerMovePath)target;
+
+        //EditorGUILayout.MinMaxSlider(ref data.nowPosMin, ref data.nowPosMax, 0.0f, 1000000.0f);
+    }
+}
---
+++ Assets/Scripts/Stage/PlayerMovePath.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+// 移動先のパスのデータ
+[System.Serializable]
+public class ChangeNextPathData
+{
+    // 左右どちらのキーで移動するか
+    public PlayerMovePath.MoveDir moveDir;
+    // 移動先のパス
+    public CinemachinePathBase changePath;
+    // 移動先パスの移動可能範囲のPosの最小値
+    public float changePosMin;
+    // 移動先パスの移動可能範囲のPosの最大値
+    public float changePosMax;
+}
+
+[System.Serializable]
+public class PlayerMoveData2
+{
+    // 移動元のパス
+    public CinemachinePathBase nowPath;
+    // 移動元の移動可能範囲のPosの最小値
+    public float nowPosMin;
+    // 移動元の移動可能範囲のPosの最小値
+    public float nowPosMax;
+
+    [SerializeField,]
+    List<ChangeNextPathData> changeNextPathDataList = new List<ChangeNextPathData>();
+}
+
+
+public class PlayerMovePath : MonoBehaviour
+{
+    // 移動キー
+    public enum MoveDir
+    {
+        None,
+        Right,
+        Left
+    }
+    [SerializeField]
+    List<PlayerMoveData2> playerMoveData2s = new List<PlayerMoveData2>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
