7fecf4535d835a308520d0cfe66b65e055adaf94
Restly <liknlie@gmail.com>
2021-01-13 12:17:00 +0900

    Boss1AnimationSet

--- Assets/Scripts/Enemy/Boss/Boss1Animation.cs
+++ Assets/Scripts/Enemy/Boss/Boss1Animation.cs
@@ -4,53 +4,27 @@ using UnityEngine;
 
 public class Boss1Animation : MonoBehaviour
 {
-    private CharacterController animController = null;
-
-    private Animator animator = null;
-
     private GameObject bossModel = null;
-
-    [SerializeField]
-    private float damageAnimTimer = 0.0f;
-    private float timer = 0.0f;
-    private bool isDamage = false;
 
     // Start is called before the first frame update
     void Start()
     {
-
         bossModel = transform.GetChild(0).gameObject;
-
-        animController = bossModel.GetComponent<CharacterController>();
-        animator = bossModel.GetComponent<Animator>();
-
-        timer = damageAnimTimer;
+        bossModel.GetComponent<Animator>().Play("AttackAnim");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (isDamage)
-        //{
-        //    timer--;
 
-        //    if (timer < 0.0f)
-        //    {
-        //        isDamage = false;
-        //        animator.SetBool("Damage", false);
-        //        timer = damageAnimTimer;
-        //    }
-        //}
-        //Debug.Log(isDamage);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "EnemyBullet")
         {
-            if (other.GetComponent<EnemyBulletStateController>().lastStateEnum == EnemyBulletStateController.BulletStateEnum.Parry)
+            if (other.GetComponent<EnemyBulletStateController>().LastStateEnum == EnemyBulletStateController.BulletStateEnum.Parry)
                 bossModel.GetComponent<Animator>().Play("DamageAnim");
         }
     }
-
 }
--- Assets/Scripts/Enemy/Boss/EnemyBulletStateController.cs
+++ Assets/Scripts/Enemy/Boss/EnemyBulletStateController.cs
@@ -15,7 +15,8 @@ public class EnemyBulletStateController : MonoBehaviour
     }
 
     // 自身のステート
-    public BulletStateEnum lastStateEnum = BulletStateEnum.EnemyBullet;
+    private BulletStateEnum lastStateEnum = BulletStateEnum.EnemyBullet;
+    public BulletStateEnum LastStateEnum { get { return lastStateEnum; } }
     private BulletState activeState;
     private BulletState[] stateList = new BulletState[2];
 
