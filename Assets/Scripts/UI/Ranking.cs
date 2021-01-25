f1eacb697249d2104e54c9547af8791820cb0e60
eit158 <nm7.jda.abc@gmail.com>
2021-01-25 14:55:44 +0900

    RankingSummary

--- Assets/Scripts/UI/Ranking.cs
+++ Assets/Scripts/UI/Ranking.cs
@@ -2,22 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System.Text;
 
 public class Ranking : MonoBehaviour
 {
-    [SerializeField, Header("登録ボタン")]
-    private Button subscribe = null;
+    
+
+
     [SerializeField, Header("ハイスコア")]
     private int score = 0;
-    [SerializeField, Header("名前親")]
-    private Text[] namesText = new Text[5];
+
+ 
     [SerializeField, Header("表示させるテキスト")]
     private Text[] rankingText = new Text[5];
-    //[SerializeField, Header("Field")]
-    //private InputField inputField = null;
+
+    private StreamWriter sw;
+
+
+  
+
+    
 
     private int highScore = 0;
     private string[] ranking = { "ランキング1位", "ランキング2位", "ランキング3位", "ランキング4位", "ランキング5位" };
+    int[] rankingValue = new int[5];
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,9 +37,8 @@ public class Ranking : MonoBehaviour
         
         highScore = PlayerPrefs.GetInt(ranking[0]);
 
-        Show();
-
-        subscribe.onClick.AddListener(() => 
+    
+        //subscribe.onClick.AddListener(() => 
         {
             int j = 0;
             for (int i = 0; i < ranking.Length; i++)
@@ -41,33 +50,15 @@ public class Ranking : MonoBehaviour
                     break;
                 }
             }
-            Show();
+           // Show();
             PlayerPrefs.Save();
 
-        });
+        };
     }
 
-    private void Show()
-    {
-        for (int i = 0; i < rankingText.Length; i++)
-        {
-            namesText[i].text = PlayerPrefs.GetString(ranking[i],"not player");
-            rankingText[i].text = PlayerPrefs.GetInt(ranking[i], 0).ToString();
-        }
+    
 
-        
-    }
-
-    private void SwapName(string name, ref int index)
-    {
-        var temp = PlayerPrefs.GetString(PlayerPrefs.GetInt(ranking[index]).ToString(), "not player");
-        PlayerPrefs.SetString(PlayerPrefs.GetInt(ranking[index]).ToString(), name);
-        if (index < ranking.Length - 1)
-        {
-            index++;
-            SwapName(temp, ref index);
-        }
-    }
+    
 
     private void SwapScore(int score, ref int index)
     {
---
+++ Assets/Scripts/UI/ScoreRanking.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScoreRanking : MonoBehaviour
+{
+    [SerializeField, Header("数値")]
+    private int point;
+
+    string[] ranking = { "ランキング1位", "ランキング2位", "ランキング3位", "ランキング4位", "ランキング5位" };
+    int[] rankingValue = new int[5];
+
+    [SerializeField, Header("表示させるテキスト")]
+    Text[] rankingText = new Text[5];
+
+   
+
+    //獲得スコアの表示
+    public Text scoreText;
+
+    private int score = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        //スコアを入れる
+        score = Score.GetScore;
+
+        GetRanking();
+
+        SetRanking(point);
+
+
+        for (int i = 0; i < rankingText.Length; i++)
+        {
+            rankingText[i].text = rankingValue[i].ToString();
+
+        }
+        
+    }
+
+    void Update()
+    {
+        //獲得スコア表示
+        scoreText.text = point.ToString();
+    }
+
+
+        /// <summary>
+        /// ランキング呼び出し
+        /// </summary>
+        void GetRanking()
+    {
+        //ランキング呼び出し
+        for (int i = 0; i < ranking.Length; i++)
+        {
+            rankingValue[i] = PlayerPrefs.GetInt(ranking[i]);
+            
+        }
+    }
+    /// <summary>
+    /// ランキング書き込み
+    /// </summary>
+    void SetRanking(int _value)
+    {
+        bool setColorFlag = false;
+        //書き込み用
+        for (int i = 0; i < ranking.Length; i++)
+        {
+            //取得した値とRankingの値を比較して入れ替え
+            if (_value > rankingValue[i])
+            {
+                var change = rankingValue[i];
+                rankingValue[i] = _value;
+                _value = change;
+                if(!setColorFlag)
+                {
+                    rankingText[i].color = Color.red;
+                    setColorFlag = true;
+                }
+            }
+            
+        }
+
+        //入れ替えた値を保存
+        for (int i = 0; i < ranking.Length; i++)
+        {
+            PlayerPrefs.SetInt(ranking[i], rankingValue[i]);
+        }
+    }
+}
+
