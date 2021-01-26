faf5eaeff460b5b91da3f9a5c75d722aca297d48
liknlie7 <liknlie@gmail.com>
2021-01-26 13:00:30 +0900

    Animation Update4

---
+++ Assets/Scripts/Test/AnimationTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            GetComponent<Animator>().Play("Attack");
+        }
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            GetComponent<Animator>().Play("Damage");
+        }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            GetComponent<Animator>().Play("Drawal");
+        }
+    }
+}
