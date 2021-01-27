5c82e51369ed18a9c171aef35f4ae6fd05598397
kkm-sgr <kakami.suguru@gmail.com>
2021-01-28 01:39:28 +0900

    add Concentrated Line

---
+++ Assets/Scripts/Effect/ConcentratedLineModuleController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConcentratedLineModuleController : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem particleSystem = null;
+
+    ParticleSystem.MainModule mainModule;
+    ParticleSystem.EmissionModule emissionModule;
+    ParticleSystem.ShapeModule shapeModule;
+
+    // エフェクトの数調整用
+    [SerializeField, Range(1, 100)]
+    float rate = 1.0f;
+    // エフェクトの出現から消えるまでのスピード調整用
+    [SerializeField, Range(0, 2)]
+    float speed = 1.0f;
+    // エフェクトの奥行きのランダム調整用
+    [SerializeField, Range(0, 1)]
+    float rand = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // メインモジュール
+        mainModule = particleSystem.main;
+        SetSimulationSpeed(speed);
+        // エミッションモジュール
+        emissionModule = particleSystem.emission;
+        SetRateOverTime(rate);
+        // シェイプモジュール
+        shapeModule = particleSystem.shape;
+        SetRandomDirection(rand);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // エフェクトの数
+    public void SetRateOverTime(float rateNum)
+    {
+        emissionModule.rateOverTime = rateNum;
+    }
+
+    // シミュレーションスピード
+    public void SetSimulationSpeed(float speedNum)
+    {
+        mainModule.simulationSpeed = speedNum;
+    }
+
+    // ランダムの大きさ
+    public void SetRandomDirection(float randNum)
+    {
+        shapeModule.randomDirectionAmount = randNum;
+    }
+}
---
+++ Assets/Scripts/Effect/ConcentratedLineParticlesController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConcentratedLineParticlesController : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem particleSystem;
+    [SerializeField]
+    public float speed = 50;
+
+    private ParticleSystem.Particle[] particles;
+
+    private Vector3 tmp;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // 必要な場合のみ配列を作成しなおす
+        int maxParticles = particleSystem.main.maxParticles;
+
+        if (particles == null || particles.Length < maxParticles)
+        {
+            particles = new ParticleSystem.Particle[maxParticles];
+        }
+
+        // 現在のパーティクルを取得する
+        int particleNum = particleSystem.GetParticles(particles);
+
+        //パーティクルひとつひとつの処理
+        for (int i = 0; i < particleNum; i++)
+        {
+            tmp = particles[i].position;
+            // ひとつひとつのパーティクルを後ろに動かす
+            tmp.z -= Time.deltaTime * speed;
+
+            particles[i].position = tmp;
+        }
+
+        // 変更を適用する
+        particleSystem.SetParticles(particles, particleNum);
+    }
+}
