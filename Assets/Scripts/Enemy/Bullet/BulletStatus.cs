43da4737264cdd7821728c3fd4a27c5b97153ff4
Denobox <a>
2021-02-02 12:47:27 +0900

    EnemyBulletStatus

---
+++ Assets/Scripts/Enemy/Bullet/BulletStatus.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletStatus : MonoBehaviour
+{
+    public enum Dir
+    {
+        None,
+        Right,
+        Left
+    }
+
+    public Dir dir = Dir.None;
+}
