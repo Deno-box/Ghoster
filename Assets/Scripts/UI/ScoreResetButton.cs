06232aa0332631be57983b7f396f9336e73b740b
Denobox <a>
2021-02-17 10:59:07 +0900

    comp

---
+++ Assets/Scripts/UI/ScoreResetButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreResetButton : MonoBehaviour
+{
+    public void OnClickButton()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
